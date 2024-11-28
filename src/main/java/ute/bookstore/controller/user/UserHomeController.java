d56e6fb2350da41f2a4b28ab3ca9276ede46721f
maihonghai <22133014@student.hcmute.edu.vn>
2024-11-28 22:00:24 +0700

    update giao dien user

--- src/main/java/ute/bookstore/controller/user/UserHomeController.java
+++ src/main/java/ute/bookstore/controller/user/UserHomeController.java
@@ -2,17 +2,39 @@ package ute.bookstore.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserHomeController {
 	
-	@GetMapping("/user")
+	@GetMapping({"","/home"})
 	public String home() {
-		return "user/home";
+		return "user/home.html";
 	}
 	@GetMapping("/cart")
 	public String getCartPage() {
 		return "user/cart/show";
 	}
+	@GetMapping("/shopping")
+	public String getShoppingPage() {
+		return "user/user-shopping";
+	}
+	@GetMapping("/updateInfo")
+	public String getUpdateInfoPage() {
+		return "user/user-edit";
+	}
+	@GetMapping("/manageDeliveryAddresses")
+	public String getDeliveryAddressesPage() {
+		return "user/user-manage-address";
+	}
 	
+	@GetMapping("/updatePassword")
+	public String getUpdatePasswordPage() {
+		return "user/user-edit-password";
+	}
+	@GetMapping("/purchaseHistory")
+	public String getPurchaseHistoryPage() {
+		return "user/user-purchase-history";
+	}
 }
