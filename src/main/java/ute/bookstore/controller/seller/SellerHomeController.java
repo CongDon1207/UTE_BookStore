c90bdb8e62b399c2d8377574770247bf33651bd8
CongDon1207 <22133013@student.hcmute.edu.vn>
2024-11-29 20:44:25 +0700

    doi entity shop va layout cho seller

--- src/main/java/ute/bookstore/controller/seller/SellerHomeController.java
+++ src/main/java/ute/bookstore/controller/seller/SellerHomeController.java
@@ -2,12 +2,17 @@ package ute.bookstore.controller.seller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import ute.bookstore.entity.Shop;
+
 
 @Controller
 public class SellerHomeController {
-	
-	@GetMapping("seller")
-	public String Home() {
-		return "seller/home";
-	}
+    
+    @GetMapping("seller")
+    public String Home(Model model) {
+        // Thêm shop vào model
+        model.addAttribute("shop", new Shop()); // Hoặc lấy từ service
+        return "seller/home";
+    }
 }
