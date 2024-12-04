f7870fff1e21a0ac06bbc622c5c9096a8f15229d
maihonghai <22133014@student.hcmute.edu.vn>
2024-12-04 23:00:29 +0700

    capnhatthongtin,doimk,quanlydiachi user

--- src/main/java/ute/bookstore/repository/UserRepository.java
+++ src/main/java/ute/bookstore/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package ute.bookstore.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import ute.bookstore.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findById(Long id);
+	boolean existsByEmailAndIdNot(String email, Long id);
+	
+	
     
 }
