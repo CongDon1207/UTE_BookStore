1053b1973f6ee6e37b760d09c250734a4efdff4d
maihonghai <22133014@student.hcmute.edu.vn>
2024-12-06 01:51:30 +0700

    Trang chu, trang chi tiet..

--- src/main/java/ute/bookstore/service/IBookService.java
+++ src/main/java/ute/bookstore/service/IBookService.java
@@ -1,6 +1,7 @@
 package ute.bookstore.service;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 import ute.bookstore.entity.Book;
 import ute.bookstore.entity.Category;
@@ -20,4 +21,7 @@ public interface IBookService {
 	void deleteBook(Long id);
 	
 	List<Book> getAllBooks();
+	Page<Book> getAllBooks(Pageable pageable); // Thêm tham số Pageable
+	List<Book> getFeaturedBooks();
+	 Page<Book> getBooks(Long categoryId, String sortBy, String sortDir, int page, int size);
 }
